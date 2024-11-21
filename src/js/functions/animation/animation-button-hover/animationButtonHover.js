bb78ece83ce732aeb89742dc3f9fc9678174eab7
veronika <mihotinaw@gmail.com>
2024-11-21 21:55:18 +0300

    Create base project.

---
+++ src/js/functions/animation/animation-button-hover/animationButtonHover.js
@@ -0,0 +1,11 @@
+import Cursor from './cursor';
+import ButtonCtrl from './buttonCtrl.js';
+
+export const animationButtonHover = () => {
+  // initialize custom cursor
+  const cursor = new Cursor(document.querySelector('.cursor'));
+  const button = new ButtonCtrl(document.querySelector('.btn-common'));
+
+  button.on('enter', () => cursor.enter());
+  button.on('leave', () => cursor.leave());
+}
